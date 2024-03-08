f2be45fdb2222da76e3d28ad4b5f9d0e024a89f0
Danila <danila.alpatov@aviasales.ru>
2024-03-08 10:44:27 +0300

    fix(pwa): update manifest

--- vite.config.ts
+++ vite.config.ts
@@ -12,19 +12,36 @@ export default defineConfig({
       manifest: {
         name: 'Tea Time',
         short_name: 'TeaTime',
+        start_url: 'index.html',
+        id: '/tea-time/index.html',
+        display: 'standalone',
+        background_color: '#000',
+        lang: 'en-US',
+        scope: '/tea-time/',
         description: 'Tea brewing infusion timer',
         theme_color: '#000',
-        display: 'standalone',
-        start_url: 'index.html',
-        lang: 'en-US',
-        // screenshots: [
-        //   { src: 'horizontal.png', form_factor: 'wide', type: 'image/png' },
-        //   { src: 'vertical.png', form_factor: 'narrow', type: 'image/png' },
-        // ],
         icons: [
           {
             src: 'maskable.svg',
             sizes: 'any',
+            type: 'image/svg',
+            purpose: 'maskable',
+          },
+          {
+            src: 'maskable.svg',
+            sizes: '512x512',
+            type: 'image/svg',
+            purpose: 'maskable',
+          },
+          {
+            src: 'icon-144.svg',
+            sizes: '144x144',
+            type: 'image/svg',
+            purpose: 'any',
+          },
+          {
+            src: 'icon-72.svg',
+            sizes: '72x72',
             type: 'image/svg',
             purpose: 'maskable',
           },
