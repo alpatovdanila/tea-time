a52aaf8b5002cce5cb5916c1f457ff868808eb58
Danila <danila.alpatov@aviasales.ru>
2024-03-12 21:47:23 +0300

    fix(model): modal window hide

--- src/model/ui.ts
+++ src/model/ui.ts
@@ -2,7 +2,7 @@ import { createEvent, createStore } from '../lib/nanite'
 import { AlertProps } from '../ui/modal/alert'
 
 export const setModalWindow = createEvent<AlertProps | null>()
-export const hideModalWindow = setModalWindow.with(null)
+export const hideModalWindow = () => setModalWindow(null)
 
 export const $modalWindow = createStore<AlertProps | null>(null)
 
